9627acddd73590456db3b583786a18a5f4bc89cc
its-bismay <2023ugee070@nitjsr.ac.in>
2025-09-11 12:18:58 +0530

    unsplash api setup

--- app/(main)/editor/[projectId]/_components/_tools/BackgroundControls.jsx
+++ app/(main)/editor/[projectId]/_components/_tools/BackgroundControls.jsx
@@ -1,4 +1,7 @@
-const BackgroundControls = () => {
+
+const UNSPLASH_ACCESS_KEY = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
+const UNSPLASH_API_URL = "https://api.unsplash.com"
+const BackgroundControls = ({project}) => {
   return (
     <div>BackgroundControls</div>
   )
